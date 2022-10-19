4c62737e7433af615365caf86a39dff2b9ab8256
chigaevi <chigaev.i@gmail.com>
2022-10-19 19:05:35 +0700

    edit 4 ADV SELECT

--- ADV SELECT.sql
+++ ADV SELECT.sql
@@ -2,7 +2,6 @@
 select genre_id, count(*)  from musician_genre mg
 group by genre_id
 
-
 -- 2.количество треков, вошедших в альбомы 2019-2020 годов;
 select album_name, count(*)  from track
 where album_name = (select album_id from album where release_year >= 2019 and release_year <= 2020)
@@ -17,13 +16,11 @@ select musician_name from track t
 join album a on a.album_id = t.album_name
 where release_year != 2020;
 
-
 --5. названия сборников, в которых присутствует конкретный исполнитель (Linkin Park у которых id = 10);
 select collection_name, musician_name from collection c
 join collection_track ct on ct.collection_id = c.collection_id 
 join track t on ct.track_id = t.track_id
 where musician_name = 10
-
 
 --6. название альбомов, в которых присутствуют исполнители более 1 жанра;
 select album_name from album a 
@@ -33,7 +30,17 @@ group by album_name
 having count(genre_id) > 1
 
 --7. наименование треков, которые не входят в сборники;
+select track_name  from track t
+left join collection_track ct on ct.track_id = t.track_id
+where collection_id is null
+
 --8. исполнителя(-ей), написавшего самый короткий по продолжительности трек (теоретически таких треков может быть несколько);
+select m.musician_name from musician m 
+join track t on t.musician_name = m.musician_id
+where track_duration = (select min(track_duration) from track t2 )
+
 --9. название альбомов, содержащих наименьшее количество треков.
-
-
+select a.album_name, count(track_id) from album a
+join track t on t.album_name = a.album_id 
+group by a.album_name 
+having count(track_id) = 1 
